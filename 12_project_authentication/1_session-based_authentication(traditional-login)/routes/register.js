21cabff8c55ed85cbf5da60b899284352d2f1060
tanvir ahammad <tanvirahammadjoy@gmail.com>
2025-04-22 14:53:41 +0800

    refactor session-based authentication to support multiple content types for registration

--- 12_project_authentication/1_session-based_authentication(traditional-login)/routes/register.js
+++ 12_project_authentication/1_session-based_authentication(traditional-login)/routes/register.js
@@ -1,10 +1,26 @@
 const { addUser, findUser } = require("../userStore");
+const querystring = require("querystring"); // Importing querystring to parse URL-encoded data
 
 const register = (req, res) => {
   let body = "";
   req.on("data", (chunk) => (body += chunk.toString()));
   req.on("end", () => {
-    const { username, password } = JSON.parse(body);
+    // Parse the body as JSON
+    // const { username, password } = JSON.parse(body);
+    // Alternatively, if you want to parse URL-encoded data, use the following line instead:
+    // const { username, password } = querystring.parse(body);
+
+    let username, password;
+    if (req.headers["content-type"] === "application/json") {
+      ({ username, password } = JSON.parse(body));
+    } else if (
+      req.headers["content-type"] === "application/x-www-form-urlencoded"
+    ) {
+      ({ username, password } = querystring.parse(body));
+    } else {
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      return res.end("Unsupported content type");
+    }
 
     if (!username || !password) {
       res.writeHead(400, { "Content-Type": "text/plain" });
